0c7e49745ddddfb17754eed7944ee18fd20c015f
Felix Samsonov <german1samsonov@gmail.com>
2024-11-21 15:35:10 +0200

    add checking numbers

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System.Text;
 Console.OutputEncoding = Encoding.UTF8;
 
-while (true)//щоб не перезапускати постійно консоль
+while (true)
 {
     Console.WriteLine("Введіть перше число для обчислення");
     string inputFirstNumber = Console.ReadLine();
@@ -9,37 +9,50 @@ while (true)//щоб не перезапускати постійно консо
     {
         break;
     }
-    decimal firstNumber = decimal.Parse(inputFirstNumber);                                                     
-    Console.WriteLine("Ведіть математичну дію");
+    decimal firstNumber;
+    if (decimal.TryParse(inputFirstNumber, out firstNumber))
+    {
+        Console.WriteLine("Ведіть математичну дію");
+    }
+    else
+    {
+        Console.WriteLine($"Не корректний формат числа '{inputFirstNumber}', спробуй ще");
+        continue;
+    }
     string operation = Console.ReadLine();
     Console.WriteLine("Введіть друге число");
     string inputSecondNumber = Console.ReadLine();
-    decimal secondNumber = decimal.Parse(inputSecondNumber);
-
-    switch (operation)
+    decimal secondNumber;
+    if (decimal.TryParse(inputSecondNumber, out secondNumber))
     {
-        case "+":
-            Console.WriteLine($"Результат: {firstNumber + secondNumber}");
-            break;
-        case "-":
-            Console.WriteLine($"Результат: {firstNumber - secondNumber}");
-            break;
-        case "/":
-             try
-             {
-            Console.WriteLine($"Результат: {firstNumber / secondNumber}");
-             }
-              catch (DivideByZeroException ex)
-             {
-            Console.WriteLine("На нуль ділити не можна/" + ex.Message);
-            }
-            break;
-        case "*":
-            Console.WriteLine($"Результат: {firstNumber * secondNumber}"); 
-            break;
-        default: 
-            Console.WriteLine("Введена не математична дія");
-            break;
-            }
+        switch (operation)
+        {
+            case "+":
+                Console.WriteLine($"Результат: {firstNumber + secondNumber}");
+                break;
+            case "-":
+                Console.WriteLine($"Результат: {firstNumber - secondNumber}");
+                break;
+            case "/":
+                try
+                {
+                    Console.WriteLine($"Результат: {firstNumber / secondNumber}");
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine("На нуль ділити не можна/" + ex.Message);
+                }
+                break;
+            case "*":
+                Console.WriteLine($"Результат: {firstNumber * secondNumber}");
+                break;
+            default:
+                Console.WriteLine("Введена не математична дія");
+                break;
+        }
+    }
+    else
+    {
+        Console.WriteLine($"Не корректний формат числа '{inputSecondNumber}'");
+    }
 }
-
