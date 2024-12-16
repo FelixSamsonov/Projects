39b2c714b02fd8d8743e35cda082953ad5422d8c
Felix Samsonov <german1samsonov@gmail.com>
2024-12-16 23:08:11 +0200

    HW Method Fibonachi

---
+++ methods/methods/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine("Введіть індекс числа послідовності Фібоначчі");
+            if (int.TryParse(Console.ReadLine(), out int numb))
+            {
+                Console.WriteLine($"Число Фібоначчі для індексу {numb}: {Fibonachi(numb)}");
+            }
+            else
+            {
+                Console.WriteLine("Введене не числове значення !!!");
+            }
+        }
+        static int Fibonachi(int numb)
+        {
+            if (numb < 0)
+                throw new ArgumentException(nameof(numb));
+            if (numb == 0) return 0;
+            if (numb <= 3) return 1;
+
+            return Fibonachi(numb - 1) + Fibonachi(numb - 2);
+        }
+    }
+
+}
+
