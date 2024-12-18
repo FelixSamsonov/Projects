7f3c46ceb64b87e85e542b809b4240be587d322e
Felix Samsonov <german1samsonov@gmail.com>
2024-12-18 21:41:05 +0200

    HW Methods

--- methods/methods/Program.cs
+++ methods/methods/Program.cs
@@ -7,18 +7,21 @@ namespace methods
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
-
             Console.WriteLine("Введіть індекс числа послідовності Фібоначчі");
             if (int.TryParse(Console.ReadLine(), out int numb))
             {
-                Console.WriteLine($"Число Фібоначчі для індексу {numb}: {Fibonachi(numb)}");
+                Console.WriteLine($"Число Фібоначчі для індексу {numb}: {OrderFibonachi.Fibonachi(numb)}");
             }
             else
             {
                 Console.WriteLine("Введене не числове значення !!!");
             }
         }
-        static int Fibonachi(int numb)
+
+    }
+    public class OrderFibonachi
+    {
+        public static int Fibonachi(int numb)
         {
             if (numb < 0)
                 throw new ArgumentException(nameof(numb));
